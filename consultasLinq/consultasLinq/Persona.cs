dbf9e32c31e91cf6abdfda002de37e972db41304
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2024-02-13 23:08:09 -0600

    consultas con linq

---
+++ consultasLinq/consultasLinq/Persona.cs
@@ -0,0 +1,8 @@
+﻿namespace consultasLinq
+{
+     class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
---
+++ consultasLinq/consultasLinq/Program.cs
@@ -0,0 +1,19 @@
+﻿// Creación de una lista de personas
+using consultasLinq;
+
+List<Persona> personas = new List<Persona>
+{
+    new Persona { Nombre = "Juan", Edad = 30 },
+    new Persona { Nombre = "María", Edad = 25 },
+    new Persona { Nombre = "Pedro", Edad = 35 }
+};
+
+// Consulta LINQ con select para seleccionar solo los
+// nombres de las personas
+var nombres = from persona in personas
+              select persona.Nombre;
+
+// Iterar sobre los nombres y mostrarlos en la consola
+foreach (var nombre in nombres){
+    Console.WriteLine(nombre);
+}
